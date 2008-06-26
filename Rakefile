a1d803cb197a9aab9f68a5b82b68c7270855bfbf
Ehren Murdick <ehren@theedgecase.com>
2008-06-26 15:33:53 -0400

    working on gem file structure

---
+++ Rakefile
@@ -0,0 +1,16 @@
+spec = Gem::Specification.new do |s| 
+  s.name = "RCss"
+  s.version = "0.0.1"
+  s.author = "Ehren Murdick"
+  s.email = "ehren.murdick@gmail.com"
+  s.homepage = "http://theedgecase.com"
+  s.platform = Gem::Platform::RUBY
+  s.summary = "RCss: Ruby CSS"
+  s.files = FileList["{bin,lib}/**/*"].to_a
+  s.require_path = "lib"
+  s.autorequire = "selector"
+  s.autorequire = "rcss"
+  s.has_rdoc = true
+  s.extra_rdoc_files = ["README"]
+end
+
---
+++ lib/rcss.rb
@@ -0,0 +1,49 @@
+class Rcss
+  attr_accessor :selectors
+  def initialize
+    @output = ""
+    @selectors = {}
+    @headers = []
+  end
+
+  def css &block
+    instance_eval &block
+  end
+
+  def header str
+    @headers << str
+  end
+
+  def import path
+    header "@import \"#{path}\";"
+  end
+
+  def any id_or_class, &block
+    if id_or_class.to_s == ''
+      send("*", "", &block)
+    else
+      method_missing("", id_or_class, &block)
+    end
+  end
+
+  def method_missing method_name, id_or_class = '', &block
+    if id_or_class =~ /#/
+      s = Selector.new '', id_or_class, self
+    else
+      s = Selector.new method_name, id_or_class, self
+    end
+    s = @selectors[s.name] if @selectors[s.name]
+    s.instance_eval &block
+    @selectors[s.name] = s
+  end
+
+  def render(minified = false)
+    res = ""
+    res << @headers.join("\n")
+    res << "\n"
+    @selectors.each_value do |s|
+      res << s.render(minified)
+    end
+    res
+  end
+end
